630bf657001e19382993410cdbfe345f0809d27b
hajarhuseynova <hajarih@code.edu.az>
2025-03-28 11:43:27 +0400

    Add: About page and Hero component

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-
+import { Hero } from '../components/index'
 const Landing = () => {
   return (
-    <div>
-      <p>Landing</p>
-    </div>
+    <>
+      <Hero />
+    </>
   )
 }
 
