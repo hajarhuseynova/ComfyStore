b7c52708bce9b920498eac4f2d9050d7adfd9920
hajarhuseynova <hajarih@code.edu.az>
2025-03-29 10:45:33 +0400

    Add:Single Product page and Error Element

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,9 @@ import {
   Checkout,
   Landing,
 } from './pages'
+import { ErrorElement } from './components'
+import { loader as landingLoader } from './pages/Landing'
+import { loader as singleProductLoader } from './pages/SingleProduct'
 
 const router = createBrowserRouter([
   {
@@ -22,11 +25,21 @@ const router = createBrowserRouter([
     element: <HomeLayout />,
     errorElement: <Error />,
     children: [
-      { index: true, element: <Landing /> },
+      {
+        index: true,
+        element: <Landing />,
+        errorElement: <ErrorElement />,
+        loader: landingLoader,
+      },
       { path: 'about', element: <About /> },
       { path: 'orders', element: <Orders /> },
       { path: 'products', element: <Products /> },
-      { path: 'products/:id', element: <SingleProduct /> },
+      {
+        path: 'products/:id',
+        element: <SingleProduct />,
+        errorElement: <ErrorElement />,
+        loader: singleProductLoader,
+      },
       { path: 'cart', element: <Cart /> },
       { path: 'checkout', element: <Checkout /> },
     ],
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-import { Hero } from '../components/index'
+import { FeaturedProducts, Hero } from '../components/index'
+import { customFetch } from '../utils/index'
+
+const url = '/products?featured=true'
+
+export const loader = async () => {
+  const response = await customFetch(url)
+  const products = response.data.data
+  return { products }
+}
 const Landing = () => {
   return (
     <>
       <Hero />
+      <FeaturedProducts />
     </>
   )
 }
